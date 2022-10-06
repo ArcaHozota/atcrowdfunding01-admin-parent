655ff0e811a4bd6b6c7caeb17c472f78ea78be6b
ArcaHozota <arkamahozota@gmail.com>
2022-10-07 07:44:33 +0900

    1.006c

--- atcrowdfunding03-admin-component/src/main/java/com/atdaiwa/crowd/service/api/AdminService.java
+++ atcrowdfunding03-admin-component/src/main/java/com/atdaiwa/crowd/service/api/AdminService.java
@@ -7,19 +7,66 @@ import com.github.pagehelper.PageInfo;
 
 public interface AdminService {
 
+    /**
+     * 儲存用戶信息；
+     *
+     * @param admin 用戶對象；
+     */
     void saveAdmin(Admin admin);
 
+    /**
+     * 獲取所有用戶信息；
+     *
+     * @return List<Admin>
+     */
     List<Admin> getAll();
 
+    /**
+     * 根據登錄賬戶查找用戶信息；
+     *
+     * @param loginAccount 用戶賬號；
+     * @param userPassword 密碼；
+     * @return Admin對象
+     */
     Admin getAdminByLoginAccount(String loginAccount, String userPassword);
 
+    /**
+     * 獲取已完成分頁用戶信息
+     *
+     * @param keyword 檢索關鍵字；
+     * @param pageNum 頁碼；
+     * @param pageSize 分頁大小；
+     * @return PageInfo<Admin>
+     */
     PageInfo<Admin> getPageInfo(String keyword, Integer pageNum, Integer pageSize);
 
+    /**
+     * 刪除用戶信息；
+     *
+     * @param adminId 用戶ID；
+     */
     void remove(Integer adminId);
 
+    /**
+     * 通過id查找用戶信息；
+     *
+     * @param adminId 用戶ID；
+     * @return Admin對象
+     */
     Admin getAdminById(Integer adminId);
 
+    /**
+     * 更新用戶信息；
+     *
+     * @param admin 用戶對象；
+     */
     void update(Admin admin);
 
+    /**
+     * 儲存用戶角色信息；
+     *
+     * @param adminId 用戶ID；
+     * @param roleIdList 用戶角色列表；
+     */
     void saveAdminRole(Integer adminId, List<Integer> roleIdList);
 }
--- atcrowdfunding03-admin-component/src/main/java/com/atdaiwa/crowd/service/api/AuthService.java
+++ atcrowdfunding03-admin-component/src/main/java/com/atdaiwa/crowd/service/api/AuthService.java
@@ -7,13 +7,25 @@ import com.atdaiwa.crowd.entity.Auth;
 
 public interface AuthService {
 
-	/**
-	 * 獲取所有權限
-	 * @return List<Auth>
-	 */
-	List<Auth> getAll();
+    /**
+     * 獲取所有權限信息；
+     *
+     * @return List<Auth>
+     */
+    List<Auth> getAll();
 
-	List<Integer> getAssignedAuthIdByRoleId(Integer roleId);
+    /**
+     * 通過角色查找已被賦予的權限信息；
+     *
+     * @param roleId 角色ID；
+     * @return List<Integer>
+     */
+    List<Integer> getAssignedAuthIdByRoleId(Integer roleId);
 
-	void saveRoleAuthRelationship(Map<String, List<Integer>> map);
+    /**
+     * 儲存角色權限關係信息；
+     *
+     * @param map 集合；
+     */
+    void saveRoleAuthRelationship(Map<String, List<Integer>> map);
 }
